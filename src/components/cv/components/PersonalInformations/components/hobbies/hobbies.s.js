2a54d97b3281d7d67a0e83f2d0601215774143d1
Marcin <elomelo302@gmail.com>
2018-09-11 22:19:25 +0200

    RWD

--- src/components/cv/components/PersonalInformations/components/hobbies/hobbies.s.js
+++ src/components/cv/components/PersonalInformations/components/hobbies/hobbies.s.js
@@ -42,7 +42,7 @@ export const HobbyIcon = glamorous.div(props => {
 
         '@media screen and (max-width:980px)': {
             width: '35%',
-            height:'75%'
+            height:'70%'
         }
     }
 })
