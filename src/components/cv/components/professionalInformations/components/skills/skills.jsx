1b2da1cbb7551984b3238dabde5bb438da3ecdcc
Marcin <elomelo302@gmail.com>
2018-09-12 19:29:43 +0200

    Style changes

--- src/components/cv/components/professionalInformations/components/skills/skills.jsx
+++ src/components/cv/components/professionalInformations/components/skills/skills.jsx
@@ -24,7 +24,7 @@ export class Skills extends Component {
                     a 15.9155 15.9155 0 0 1 0 31.831
                     a 15.9155 15.9155 0 0 1 0 -31.831"
                 fill="none"
-                stroke="#666666"
+                stroke="#d4d4d4 "
                 strokeWidth="5"
                 strokeDasharray='100'
             />
